57a2f7a941434a58e72be3cbb58dd50bbe50a024
João Celino <joaocg14@gmail.com>
2025-02-21 23:52:35 -0300

    chore: adding and return isLoggedIn status

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,15 +1,25 @@
 import { Injectable } from '@angular/core';
 import { ApiService } from './api.service';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
+import { tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
+  private isAuthenticated = false;
+
   constructor(private apiService: ApiService) {}
 
-  // Login Method
   login(credentials: { email: string; password: string }): Observable<any> {
-    return this.apiService.post('Auth/login', credentials);
+    return this.apiService.post('Auth/login', credentials).pipe(
+      tap(() => {
+        this.isAuthenticated = true; // Authenticad user def
+      })
+    );
+  }
+
+  isLoggedIn(): boolean {
+    return this.isAuthenticated; // Return auth status
   }
 }
