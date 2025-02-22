42d1f7a856a6d787ac48a5f852863fcfa34d7aa3
João Celino <joaocg14@gmail.com>
2025-02-22 00:00:06 -0300

    chore: routing costumers path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './features/auth/login/login.component';
+import { CustomerListComponent } from './features/customers/customer-list/customer-list.component';
+import { AuthGuard } from './core/guards/auth.guard';
 
 const routes: Routes = [
-  { path: 'login', component: LoginComponent },
+
   { path: '', redirectTo: '/login', pathMatch: 'full' }, // Default route
-  { path: '**', redirectTo: '/login' }, // Redirect to login for not found pages
+  { path: '**', redirectTo: '/login' }, // Redirects to login when page not found
+
+  // Protected routes
+  { path: 'customers', component: CustomerListComponent, canActivate: [AuthGuard] },
+
 ];
 
 @NgModule({
