48c2c6d7eabb22afad88e8c777237e5f6ae58c48
João Celino <joaocg14@gmail.com>
2025-02-21 22:16:16 -0300

    feat: api service generic methods

---
+++ src/app/core/services/api.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../../environments/environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ApiService {
+  private apiUrl = environment.apiUrl; 
+
+  constructor(private http: HttpClient) {}
+
+  // Generic method do Get Requests
+  get(endpoint: string) {
+    return this.http.get(`${this.apiUrl}/${endpoint}`);
+  }
+
+  // Generic method do Post Requests
+  post(endpoint: string, body: any) {
+    return this.http.post(`${this.apiUrl}/${endpoint}`, body);
+  }
+}
