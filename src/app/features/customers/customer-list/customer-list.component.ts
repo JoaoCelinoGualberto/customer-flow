41f57520a0b0e5dbe43182c3d8b59d0260380189
João Celino <joaocg14@gmail.com>
2025-02-23 18:18:23 -0300

    styles: costumer list  style

--- src/app/features/customers/customer-list/customer-list.component.ts
+++ src/app/features/customers/customer-list/customer-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CustomerService } from '../services/customer.service';
 import { Router, RouterLink } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { AuthService } from '../../../core/services/auth.service';
 
 @Component({
   selector: 'app-customer-list',
@@ -14,22 +15,46 @@ export class CustomerListComponent implements OnInit {
   customers: any[] = [];
   errorMessage: string = '';
 
-  constructor(private customerService: CustomerService) {}
+  constructor(
+    private customerService: CustomerService,
+    private authService: AuthService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
-    this.loadCustomers();
+    this.loadCustomers(); 
+    this.initTooltips();
   }
 
+  // Fetch customers from the service
   loadCustomers(): void {
     this.customerService.getCustomers().subscribe({
       next: (data) => {
-        this.customers = data.itens;
-        console.log('Clientes carregados:', this.customers);
+        this.customers = data.itens; // Assign fetched data to the customers array
+        console.log('Clientes carregados:', data);
       },
       error: (error) => {
-        this.errorMessage = 'Erro ao carregar clientes. Tente novamente mais tarde.';
         console.error('Erro ao carregar clientes:', error);
       },
+    });
+  }
+
+  // Logout the user and navigate to the login page
+  logout(): void {
+    this.authService.logout();
+    this.router.navigate(['/auth/login']);
+  }
+
+  // Navigate to the customer edit page
+  editCustomer(customerId: number): void {
+    this.router.navigate(['/customers/edit', customerId]);
+  }
+
+  // Initialize Bootstrap tooltips
+  initTooltips(): void {
+    const tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+    tooltipTriggerList.map((element) => {
+      return new (window as any).bootstrap.Tooltip(element);
     });
   }
 }
